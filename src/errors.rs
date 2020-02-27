e59ba8a73295b0645c2ef75a2da2038a6121f429
Magic Len <len@magiclen.org>
2020-02-27 22:31:23 +0800

    implement `std::error::Error` for the `ReCaptchaError` enum

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::fmt::{Display, Error as FmtError, Formatter};
+
 #[derive(Debug, Clone)]
 /// Errors of the `ReCaptcha` struct.
 pub enum ReCaptchaError {
@@ -10,3 +13,19 @@ pub enum ReCaptchaError {
     /// Errors caused by internal malfunctions.
     InternalError(String),
 }
+
+impl Display for ReCaptchaError {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
+        match self {
+            ReCaptchaError::InvalidInputSecret => f.write_str("The secret key is invalid."),
+            ReCaptchaError::InvalidReCaptchaToken => f.write_str("The reCAPTCHA token is invalid."),
+            ReCaptchaError::TimeoutOrDuplicate => {
+                f.write_str("The reCAPTCHA token is no longer valid.")
+            }
+            ReCaptchaError::InternalError(text) => f.write_str(text),
+        }
+    }
+}
+
+impl Error for ReCaptchaError {}
